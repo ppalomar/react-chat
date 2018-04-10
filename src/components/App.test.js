45cce2b21781a1d3440e4d0656e5d7c5468fa15a
PabloPalomar <ppalomar@demonware.net>
2018-04-10 18:20:36 +0100

    Added an enzyme test to check the main layout

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+
+import { shallow } from 'enzyme';
 
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders main layout properly', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.find('header')).to.have.length(0);
+      expect(wrapper.find('main')).to.have.length(1);
+      expect(wrapper.find('footer')).to.have.length(1);
+  });
 });
+
